8c9cda9d8618fc55f9b2f0a2911df10d3ec91d86
e2tox <e2tox@live.com>
2017-10-28 02:10:32 +0800

    executable path config

--- src/config.js
+++ src/config.js
@@ -7,7 +7,9 @@ const config = {
   LOG_LEVEL: process.env.LOG_LEVEL,
   ALLOW_HTTP: process.env.ALLOW_HTTP === 'true',
   DEBUG_MODE: process.env.DEBUG_MODE === 'true',
-  API_TOKENS: [],
+  PUPPETEER_CHROMIUM_PATH: process.env.PUPPETEER_CHROMIUM_PATH,
+  PUPPETEER_CHROMIUM_ARGS: ['--no-sandbox', '--disable-gpu'],
+  API_TOKENS: []
 };
 
 if (process.env.API_TOKENS) {
