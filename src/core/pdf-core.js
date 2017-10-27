88c2e4679d0e216412ef74deb763c8f44a1619e3
e2tox <e2tox@live.com>
2017-10-28 02:27:07 +0800

    fix error

--- src/core/pdf-core.js
+++ src/core/pdf-core.js
@@ -35,6 +35,7 @@ async function render(_opts = {}) {
   const browser = await puppeteer.launch({
     headless: !config.DEBUG_MODE,
     ignoreHTTPSErrors: opts.ignoreHttpsErrors,
+    executablePath: config.PUPPETEER_CHROMIUM_PATH || undefined,
     args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox'],
     sloMo: config.DEBUG_MODE ? 250 : undefined,
   });
